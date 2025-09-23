c1fd5d4ac5053ae8c83fff0c6b22b61396a03a70
Silviu7 Tanase <tanase_silviu96@yahoo.com>
2025-09-23 14:07:32 +0300

    added some code with lint errors spaces and indentations

--- Program.cs
+++ Program.cs
@@ -1,6 +1,20 @@
+using System;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello from Sample .NET App running in Docker!");
 
 app.Run();
+
+
+
+namespace SampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
