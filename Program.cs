1b9245f08fff3f24033d05cc5f1506219673cf6f
Silviu7 Tanase <tanase_silviu96@yahoo.com>
2025-09-23 18:45:24 +0300

    removed additional code

--- Program.cs
+++ Program.cs
@@ -5,14 +5,3 @@ var app = builder.Build();
 app.MapGet("/", () => "Hello from Sample .NET App running in Docker!");
 
 app.Run();
-
-namespace SampleApp
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
